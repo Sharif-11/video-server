eb2e2ed94ccc10db60611567d26a2ae344e57e24
Sharif-11 <sharifsharifulislam061@gmail.com>
2025-01-31 14:24:26 +0600

    dynamic port added

--- server.js
+++ server.js
@@ -4,4 +4,6 @@ const path = require("path");
 
 app.use("/videos", express.static(path.join(__dirname, "videos")));
 
-app.listen(3000, () => console.log("Server running on http://localhost:3000"));
+app.listen(process.env.PORT, () =>
+  console.log("Server running on http://localhost:3000")
+);
