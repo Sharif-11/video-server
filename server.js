50b32dd43af99c1f821df5617cc78f5d2a534fd6
Sharif-11 <sharifsharifulislam061@gmail.com>
2025-02-09 18:52:52 +0600

    allowed all origins

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const path = require("path");
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 const port = process.env.PORT || 3000;
 app.use("/videos", express.static(path.join(__dirname, "videos")));
 app.get("/", (req, res) => res.send("Hello World!"));
